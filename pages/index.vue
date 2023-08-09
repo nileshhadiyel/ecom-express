ef310faa49e840439df5baeaad9615f12edce889
nilesh <nilesh.h@simformsolutions.com>
2023-08-09 20:42:58 +0530

    feat: [id]

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,101 @@
 <template>
   <MainLayout>
-    <div id="IndexPage" calss="mt-4 max-w-[120px] mx-auto px-2">
-        IndexPage
+    <div id="IndexPage" calss="mt-4 max-w-[1200px] mx-auto px-2">
+      <div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-4">
+        <div v-if="products" v-for="product in products" :key="product">  
+          <ProductComponent :product="product" />
+        </div>
+      </div>
     </div>
   </MainLayout>
 </template>
 <script setup>
 import MainLayout from "../layouts/MainLayout.vue";
 
+const products = [
+  {
+    id: 1,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/88/800/800",
+    price: 9999,
+  },
+  {
+    id: 2,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/28/800/800",
+    price: 9999,
+  },
+  {
+    id: 3,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/1/800/800",
+    price: 9999,
+  },
+  {
+    id: 4,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/10/800/800",
+    price: 9999,
+  },
+  {
+    id: 4,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/12/800/800",
+    price: 9999,
+  },
+  {
+    id: 5,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/22/800/800",
+    price: 9999,
+  },
+  {
+    id: 1,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/88/800/800",
+    price: 9999,
+  },
+  {
+    id: 2,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/28/800/800",
+    price: 9999,
+  },
+  {
+    id: 3,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/1/800/800",
+    price: 9999,
+  },
+  {
+    id: 4,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/10/800/800",
+    price: 9999,
+  },
+  {
+    id: 4,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/12/800/800",
+    price: 9999,
+  },
+  {
+    id: 5,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/22/800/800",
+    price: 9999,
+  },
+];
 </script>
---
+++ pages/item/[id].vue
@@ -0,0 +1,49 @@
+<template>
+  <MainLayout>
+    <div id="ItemPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div class="md:flex gap-4 justify-between mx-auto w-full">
+        <div class="md:w-[40%]">
+          <div
+          v-if="currentImage"
+            class="rounded-lg object-fit"
+            :src="currentImage"
+          ></div>
+          <div v-if="images[0] !== ''" class="flex items-center justify-center mt-2">
+            <div v-for="image in images">
+              <img
+                @mouseover="currentImage = image"
+                @click="currentImage = image"
+                width="70"
+                class="rounded-md object-fit border-[3px] cursor-pointer"
+                :class="currentImage === image ? 'border-[#FF5353]' : ''"
+                :src="image"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from "~/layouts/MainLayout.vue";
+
+let currentImage = ref(null);
+
+onMounted(() => {
+  watchEffect(() => {
+    currentImage.value = "https://picsum.photos/id/99/800/800";
+    images.value[0] = "https://picsum.photos/id/99/800/800";
+  });
+});
+
+const images = ref([
+  "",
+  "https://picsum.photos/id/221/800/800",
+  "https://picsum.photos/id/233/800/800",
+  "https://picsum.photos/id/244/800/800",
+  "https://picsum.photos/id/67/800/800",
+  "https://picsum.photos/id/22/800/800",
+]);
+</script>
