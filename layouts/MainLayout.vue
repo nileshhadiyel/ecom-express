ff781d0c3923c3ed6f23c87d9e07f47465cf3896
nilesh <nilesh.h@simformsolutions.com>
2023-08-09 19:40:00 +0530

    feat: added files

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -39,9 +39,7 @@
             class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b"
           >
             <div v-if="true">
-              <div class="text-semibold text-[15px] my-4 px-3">
-                Welcome to EcomXpress
-              </div>
+              <div class="text-semibold text-[15px] my-4 px-3">Welcome to EcomXpress</div>
               <div class="flex items-center gap-1 px-3 mb-3">
                 <NuxtLink
                   to="/auth"
@@ -70,9 +68,94 @@
           </div>
         </li>
       </ul>
+      <div id="MainHeader" class="flex items-center w-full bg-white">
+        <div
+          class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto"
+        >
+          <NuxtLink to="/" class="min-w-[170px]">
+            <img width="170" src="/AliExpress-logo.png" />
+          </NuxtLink>
+
+          <div class="max-w-[700px] w-full md:block hidden">
+            <div class="relative">
+              <div class="flex items-center border-2 border-[#FF4646] rounded-md w-full">
+                <input
+                  class="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none"
+                  placeholder="Accessories"
+                  type="text"
+                  v-model="searchItem"
+                />
+                <Icon
+                  v-if="isSearching"
+                  name="eos-icons:loading"
+                  size="25"
+                  class="mr-2"
+                />
+                <button class="flex items-center h-[100%] p-1.5 px-2 bg-[#FF4646]">
+                  <Icon name="ph:magnifying-glass" size="20" color="#ffffff" />
+                </button>
+              </div>
+              <div class="absolute bg-white max-w-[700px] h-auto w-full">
+                <div v-if="false" class="p-1">
+                  <NuxtLink
+                    to="`/item/1`"
+                    class="flex items-center justify-between w-full cursor-pointer hover:bg-gray-100"
+                  >
+                    <div class="flex items-center">
+                      <img
+                        class="rounded-md"
+                        width="40"
+                        src="https://picsum.photos/id/82/300/320"
+                      />
+                      <div class="truncate ml-2">Testing</div>
+                    </div>
+                    <div class="truncate">$100</div>
+                  </NuxtLink>
+                </div>
+              </div>
+            </div>
+          </div>
+          <NuxtLink to="/shoppingcart" class="flex items-center">
+            <button
+              class="relative md:block hidden"
+              @mouseenter="isCartHover = true"
+              @mouseleave="isCartHover = false"
+            >
+              <span
+                class="absolute flex items-center justify-center -right-[3px] top-0 bg-[#FF4646] h-[17px] min-w-[17px] text-xs text-white px-0.5 rounded-full"
+              >
+                0
+              </span>
+              <div class="min-w-[40px]">
+                <Icon
+                  name="ph:shopping-cart-simple-light"
+                  size="33"
+                  :color="isCartHover ? '#FF4646' : ''"
+                />
+              </div>
+            </button>
+          </NuxtLink>
+          <button
+            @click="userStore.isMenuOverlay = true"
+            class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200"
+          >
+            <Icon name="radix-icons:hamburger-menu" size="33" />
+          </button>
+        </div>
+      </div>
     </div>
+
   </div>
+    <!-- <Loading v-if="userStore.isLoading" /> -->
+
+    <div class="lg:pt-[150px] md:pt-[130px] pt-[80px]" />
+
+    <slot />
+    
 </template>
 <script setup>
 let isAccountMenu = ref(false);
+let isCartHover = ref(false);
+let searchItem = ref("");
+let isSearching = ref(false);
 </script>
