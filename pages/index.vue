4d4218d0759413db9b38dca8601afd376776369e
nilesh <nilesh.h@simformsolutions.com>
2023-08-09 18:53:37 +0530

    feat: MainLayout

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <MainLayout>
+    <div id="IndexPage" calss="mt-4 max-w-[120px] mx-auto px-2">
+        IndexPage
+    </div>
+  </MainLayout>
+</template>
+<script setup>
+import MainLayout from "../layouts/MainLayout.vue";
+
+</script>
