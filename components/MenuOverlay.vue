8a0d97ce960a9abb287eab0ee24dc2706cb4d4c9
nilesh <nilesh.h@simformsolutions.com>
2023-08-10 17:05:29 +0530

    feat: auth

---
+++ components/MenuOverlay.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="MenuOverlay" class="fixed z-50 bottom-0 h-full bg-white px-3">
+        <div class="flex items-center justify-between py-5">
+            <NuxtLink to="/" @click="userStore.isMenuOverlay = false">
+                <img 
+                    width="170"
+                    src="/AliExpress-logo.png"
+                >
+            </NuxtLink>
+
+            <button 
+                @click="userStore.isMenuOverlay = false"
+                class="rounded-full p-1 hover:bg-gray-200"
+            >
+                <Icon name="mdi:close" size="30"/>
+            </button>
+        </div>
+    </div>
+</template>
+<script setup>
+import { useUserStore } from "/stores/user.js";
+const userStore = useUserStore();
+
+// const client = useSupabaseClient()
+// const user = userSupbabaseUser()
+
+const goTo = (url) => {
+    userStore.isMenuOverlay = false
+    return navigateTo('/${url}')
+}
+
+const signOut = () => {
+    client.auth.sighOut()
+    userStore.isMenuOverlay = false
+    return navigateTo('/')
+}
+
+const signIn = () => {
+    client.auth.sighOut()
+    userStore.isMenuOverlay = false
+    return navigateTo('/auth')
+}
+
+</script>
